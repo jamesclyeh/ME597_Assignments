e0ada4e0b45e1596cf40bc5435d4828ec5d23ec1
abdul-qureshi <abdulhqureshi@gmail.com>
2014-10-26 18:16:58 -0400

    Added Measurement Model with gaussian noise

--- assignment1/MeasurementModel.m
+++ assignment1/MeasurementModel.m
@@ -1,6 +1,7 @@
-function [ x, y, dec ] = MeasurementModel(actualX, actualY, actualDec )
+function  measurements = MeasurementModel(actualX, actualY, actualDec )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-    [x, y, dec] = [actualX + 0.5*randn(), actualY + 0.5*randn(), actualDec + 0.5*randn()];
+    measurements = [actualX + 0.5*randn(), ...
+        actualY + 0.5*randn(), actualDec + 10*randn()];
 end
 
