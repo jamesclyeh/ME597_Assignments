a246286899730c4c63e342079531dfe0483b7fdb
abdul-qureshi <abdulhqureshi@gmail.com>
2014-10-26 20:50:16 -0400

    Added ekf code

--- assignment1/MeasurementModel.m
+++ assignment1/MeasurementModel.m
@@ -2,6 +2,6 @@ function  measurements = MeasurementModel(actualX, actualY, actualDec )
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
     measurements = [actualX + 0.5*randn(), ...
-        actualY + 0.5*randn(), actualDec + 10*randn()];
+        actualY + 0.5*randn(), actualDec + (10 * pi()/180)*randn()];
 end
 
